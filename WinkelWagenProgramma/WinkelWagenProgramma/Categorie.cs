d3bc762905ba0d2bdd3677b1e1abd0633b55de14
Bohlken,Joep J <j.bohlken@student.fontys.nl>
2020-02-23 21:48:11 +0100

    programma toegevoegd

---
+++ WinkelWagenProgramma/WinkelWagenProgramma/Categorie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinkelWagenProgramma
+{
+    class Categorie
+    {
+        public string name = "Categorie";
+        public List<string> CategorieList = new List<string>();
+        public void addToCategorieList()
+        {
+            CategorieList.Add("Zuivel");
+            CategorieList.Add("Vlees Ware");
+            CategorieList.Add("Groente");
+            CategorieList.Add("Fruit");
+            CategorieList.Add("Snoep");
+        }
+    }
+}
---
+++ WinkelWagenProgramma/WinkelWagenProgramma/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinkelWagenProgramma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Categorie categorie = new Categorie();
+            groupBoxCategorie.Text = categorie.name;
+            categorie.addToCategorieList();
+
+            button1.Text = categorie.CategorieList[0];
+            button2.Text = categorie.CategorieList[1];
+            button3.Text = categorie.CategorieList[2];
+            button4.Text = categorie.CategorieList[3];
+            button5.Text = categorie.CategorieList[4];
+        }
+    }
+}
